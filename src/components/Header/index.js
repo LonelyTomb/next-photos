c1b4af04769af6e2167cfd8f3fd484d753f18869
Victory Garuba <garubav@gmail.com>
2021-09-22 23:42:53 +0100

    feat: create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React, {useSelector} from 'react-redux'
+import Image from 'next/image'
+import logo from '@images/logo.svg'
+import shoppingCart from '@images/shopping-cart.svg'
+import styles from '@/styles/Header.module.css'
+
+const AppHeader = () => {
+
+    const {cart} = useSelector((state) => state.products)
+
+    return (<header className={' px-6 py-6 lg:px-2xl lg:py-xl flex justify-between items-center'}>
+        <Image src={logo} className={'w-2 lg:w-xl'} alt={'app-logo'}/>
+        <div className={'flex flex-col items-end'}>
+            <div className={'w-6 lg:w-xl h-6 lg:h-xl relative'}>
+                <Image src={shoppingCart} alt={'cart-image'} layout={'fill'}/>
+            </div>
+            {cart.length > 0 && (<div className={`${styles['cart-count']} relative`}>
+                <span className={'bg-black text-white p-1 text-md'}>{cart.length}</span>
+            </div>)}
+
+        </div>
+    </header>)
+}
+
+export default AppHeader
