ca74aa7c56c459502cb1f541438186755f3d8ef2
Victory Garuba <garubav@gmail.com>
2021-09-23 17:05:07 +0100

    feat: add cart actions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,23 @@
-import React, {useSelector} from 'react-redux'
+import React, {useSelector, useDispatch} from 'react-redux'
+import {toggleCart, clearCart} from "@/redux/slices/cart";
+
 import Image from 'next/image'
 import logo from '@images/logo.svg'
 import shoppingCart from '@images/shopping-cart.svg'
+
 import styles from '@/styles/Header.module.css'
 
 const AppHeader = () => {
-
+    const dispatch = useDispatch()
     const {items} = useSelector((state) => state.cart)
+
+    const openCart = () => {
+        dispatch(toggleCart(true))
+    }
 
     return (<header className={'px-6 py-6 lg:px-2xl lg:py-xl flex justify-between items-center'}>
         <Image src={logo} className={'w-2 lg:w-xl'} alt={'app-logo'}/>
-        <div className={'flex flex-col items-end'}>
+        <button onClick={() => openCart()} className={'flex flex-col items-end'}>
             <div className={'w-6 lg:w-xl h-6 lg:h-xl relative'}>
                 <Image src={shoppingCart} alt={'cart-image'} layout={'fill'}/>
             </div>
@@ -18,7 +25,7 @@ const AppHeader = () => {
                 <span className={'bg-black text-white p-1 text-md'}>{items.length}</span>
             </div>)}
 
-        </div>
+        </button>
     </header>)
 }
 
