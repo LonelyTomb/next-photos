c2639ef734bf749b4f79afd2521c24cd78ae4643
Victory Garuba <garubav@gmail.com>
2021-09-23 14:34:13 +0100

    refactor: switch to cart store

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import styles from '@/styles/Header.module.css'
 
 const AppHeader = () => {
 
-    const {cart} = useSelector((state) => state.products)
+    const {items} = useSelector((state) => state.cart)
 
     return (<header className={'px-6 py-6 lg:px-2xl lg:py-xl flex justify-between items-center'}>
         <Image src={logo} className={'w-2 lg:w-xl'} alt={'app-logo'}/>
@@ -14,8 +14,8 @@ const AppHeader = () => {
             <div className={'w-6 lg:w-xl h-6 lg:h-xl relative'}>
                 <Image src={shoppingCart} alt={'cart-image'} layout={'fill'}/>
             </div>
-            {cart.length > 0 && (<div className={`${styles['cart-count']} relative`}>
-                <span className={'bg-black text-white p-1 text-md'}>{cart.length}</span>
+            {items.length > 0 && (<div className={`${styles['cart-count']} relative`}>
+                <span className={'bg-black text-white p-1 text-md'}>{items.length}</span>
             </div>)}
 
         </div>
