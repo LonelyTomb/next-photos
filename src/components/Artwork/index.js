53a4484f26dd8bd3777fb59ed1246b53069e3b62
Victory Garuba <garubav@gmail.com>
2021-09-23 23:43:15 +0100

    feat: plugin props data

--- src/components/Artwork/index.js
+++ src/components/Artwork/index.js
@@ -1,24 +1,38 @@
+import React, {useDispatch} from 'react-redux'
 import Image from 'next/image'
-import dummy from '@images/dummy.png'
 import styles from '@/styles/Artwork.module.css'
+import {addToCart, toggleCart} from "@/redux/slices/cart";
 
-const Artwork = ({artwork}) => {
+const Artwork = ({art}) => {
+    const dispatch = useDispatch()
+
+    const saveToCart = (item) => {
+        dispatch(addToCart(item))
+        dispatch(toggleCart(true))
+    }
+
     return (
         <div className={'relative artwork'}>
             <div className={`${styles['image-wrapper']} relative mb-3`}>
-                <span className={'absolute top-0 left-0 py-1 px-4 bg-white z-10 text-md'}>Best Seller</span>
-                <Image src={dummy} alt={'image'} layout={'fill'} objectFit={'cover'} objectPosition={'top center'}/>
-                <div className={'absolute h-full w-full group flex items-end'}>
+                {art.bestseller && (
+                    <span className={'absolute top-0 left-0 py-1 px-4 bg-white z-10 text-md'}>Best Seller</span>
+                )}
+                <div className={'relative h-full'}>
+                    <Image src={art.image && art.image.src} alt={art.image && art.image.src} layout={'fill'}
+                           objectFit={'cover'} objectPosition={'top center'}/>
+                </div>
+                <div className={'absolute h-full w-full group flex items-end top-0'}>
                     <button
+                        onClick={() => saveToCart(art)}
                         className={'bg-black font-medium text-white uppercase py-3 px-6 tracking-wide text-xl w-full group-hover:block block lg:hidden'}>Add
                         to
                         cart
                     </button>
                 </div>
             </div>
-            <p className={'text-gray font-bold text-lg mb-1 category'}>People</p>
-            <p className={'font-bold text-1xl title'}>Red Bench</p>
-            <p className={'price text-gray text-xl'}>$3.49</p>
+            <p className={'text-gray font-bold text-lg mb-1 category capitalize'}>{art.category}</p>
+            <p className={'font-bold text-1xl title capitalize'}>{art.name}</p>
+            <p className={'price text-gray text-xl'}>${art.price}</p>
         </div>
     )
 }
