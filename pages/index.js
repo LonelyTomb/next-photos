37232b5eac59b4ee166ca737085994a306a94e1e
Victory Garuba <garubav@gmail.com>
2021-09-23 23:51:29 +0100

    feat: load firbase data

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,50 @@
+import React, {useEffect, useState} from 'react'
+import {useDispatch} from 'react-redux'
 import PublicLayout from '@/layouts/public'
 import Featured from '@/components/Featured'
 import Divider from '@/components/Divider'
 import Gallery from '@/components/Gallery'
 import CartBox from "@/components/Cart";
+import {onValue} from 'firebase/database'
+import {dbRef} from "@/api";
+import {saveProducts} from '@/redux/slices/products'
 
-export default function Home() {
+export default function Home({products}) {
+    const dispatch = useDispatch()
+
+    const [artworks, setArtworks] = useState()
+
+    useEffect(() => {
+        dispatch(saveProducts(products))
+        setArtworks(products)
+    }, [dispatch, products])
+
     return (
         <PublicLayout>
-          <div className={'mx-3 lg:mx-2xl relative'}>
-              <CartBox />
-              <Divider />
-              <Featured />
-              <Divider />
-              <Gallery />
-          </div>
+            <div className={'mx-3 lg:mx-2xl relative'}>
+                <CartBox/>
+                <Divider/>
+                <Featured/>
+                <Divider/>
+                <Gallery />
+            </div>
         </PublicLayout>
     )
+}
+
+export async function getStaticProps() {
+    let products = []
+    try {
+        await onValue(dbRef('products/'), async (snapshot) => {
+            const res = await snapshot.val()
+            products = [...res]
+        });
+    } catch (e) {
+        console.log(e)
+    }
+    return {
+        props: {
+            products
+        }
+    }
 }
