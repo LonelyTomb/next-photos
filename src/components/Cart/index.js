d84fb099beba8456c8c6fde6a37b11a18765b798
Victory Garuba <garubav@gmail.com>
2021-09-23 23:43:36 +0100

    feat: plugin cart store

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,12 +2,11 @@ import React, {useSelector, useDispatch} from 'react-redux'
 import Image from 'next/image'
 
 import close from '@images/close.svg'
-import dummy from '@images/dummy.png'
 import {toggleCart, clearCart} from "@/redux/slices/cart";
 
 const CartBox = () => {
     const dispatch = useDispatch()
-    const {show} = useSelector(state => state.cart)
+    const {show, items} = useSelector(state => state.cart)
 
     const closeCart = () => {
         dispatch(toggleCart(false))
@@ -15,6 +14,8 @@ const CartBox = () => {
 
     const emptyCart = () => {
         dispatch(clearCart())
+        dispatch(toggleCart(false))
+
     }
 
     return (
@@ -23,24 +24,18 @@ const CartBox = () => {
                 <div className={'flex justify-end mb-5'}>
                     <button onClick={() => closeCart()}><Image src={close} alt={'close'}/></button>
                 </div>
-                <div className={'cart-item flex justify-between items-center mb-5'}>
-                    <div className={'w-3/5'}>
-                        <p className={'font-bold text-md'}>Samurai King Resting</p>
-                        <p className={'text-gray text-lg'}>$10000.00</p>
-                    </div>
-                    <div className={'w-2/5'}>
-                        <Image src={dummy} alt={'item'} objectFit={'cover'} objectPosition={'center'}/>
-                    </div>
-                </div>
-                <div className={'cart-item flex justify-between items-center mb-5'}>
-                    <div className={'w-3/5'}>
-                        <p className={'font-bold text-md'}>Samurai King Resting</p>
-                        <p className={'text-gray text-lg'}>$10000.00</p>
-                    </div>
-                    <div className={'w-2/5'}>
-                        <Image src={dummy} alt={'item'} objectFit={'cover'} objectPosition={'center'}/>
-                    </div>
-                </div>
+                {
+                    items.map(item=>(<div className={'cart-item flex justify-between items-center mb-5'} key={item.name}>
+                        <div className={'w-3/5'}>
+                            <p className={'font-bold text-md'}>{item.name}</p>
+                            <p className={'text-gray text-lg'}>${item.price}</p>
+                        </div>
+                        <div className={'w-2/5 relative'} style={{height: '86px'}}>
+                            <Image src={item.image.src} alt={item.image.alt} layout={'fill'} objectFit={'cover'} objectPosition={'center'}/>
+                        </div>
+                    </div>))
+                }
+
                 <hr className={'border-t'} style={{borderColor: '#C2C2C2'}}/>
                 <div className={'mt-5'}>
                     <button onClick={() => emptyCart()}
