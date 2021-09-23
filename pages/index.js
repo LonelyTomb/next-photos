5e9893978a2fad1a6a68e9da36f2e21c71a67ea6
Victory Garuba <garubav@gmail.com>
2021-09-23 17:05:24 +0100

    feat: import CartBox

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import PublicLayout from '@/layouts/public'
 import Featured from '@/components/Featured'
 import Divider from '@/components/Divider'
 import Gallery from '@/components/Gallery'
+import CartBox from "@/components/Cart";
 
 export default function Home() {
     return (
         <PublicLayout>
-          <div className={'px-3 lg:px-2xl'}>
+          <div className={'mx-3 lg:mx-2xl relative'}>
+              <CartBox />
               <Divider />
               <Featured />
               <Divider />
