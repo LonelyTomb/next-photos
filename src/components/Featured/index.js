8747e81c39077b7e754b5d7df85c5d8c92f0154d
Victory Garuba <garubav@gmail.com>
2021-09-23 23:47:32 +0100

    feat: plugin props data

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,56 +1,70 @@
+import {useDispatch, useSelector} from 'react-redux'
 import Image from 'next/image'
 import styles from '@/styles/Featured.module.css'
 import dummy from '@images/dummy.png'
-import details from '@images/details.png'
+import {useEffect, useState} from "react";
+import {addToCart, toggleCart} from "@/redux/slices/cart";
 
-const Featured = ({artwork}) => {
+const Featured = () => {
+    const dispatch = useDispatch()
+
+    const [artwork, setArtwork] = useState({})
+    const {products} = useSelector(state => state.products)
+
+    useEffect(() => {
+        const filtered = products.filter(product => product.featured)
+        const selected = filtered.length && filtered[0] || {}
+        setArtwork({...selected})
+    }, [products])
+
+    const saveToCart = (item) => {
+        dispatch(addToCart(item))
+        dispatch(toggleCart(true))
+    }
+
+
     return (
         <section className={'py-6 lg:py-xl'}>
             <div className={'flex justify-between items-center mb-6'}>
-                <span className={'font-bold text-1xl'}>Samurai King Resting</span>
+                <span className={'font-bold text-1xl capitalize'}>{artwork.name}</span>
                 <button
-                    className={'bg-black font-medium text-white uppercase py-4 px-6 tracking-wide text-xl hidden lg:inline'}>Add
-                    to
-                    cart
+                    onClick={() => saveToCart(artwork)}
+                    className={'bg-black font-medium text-white uppercase py-4 px-6 tracking-wide text-xl hidden lg:inline'}>
+                    Add to cart
                 </button>
             </div>
             <div className={`relative w-100 ${styles['featured-image']} mb-6`}>
-                <Image src={dummy} layout={'fill'} alt={'featured'} objectFit={'cover'} objectPosition={'center'}/>
+                <Image src={artwork.image && artwork.image.src || dummy} layout={'fill'}
+                       alt={artwork.image && artwork.image.alt}
+                       objectFit={'cover'} objectPosition={'center'}/>
                 <span className={'absolute bottom-0 left-0 bg-white font-bold py-5 px-xl text-base lg:text-lg'}>Photo of the Day</span>
             </div>
             <section className={'grid grid-cols-1 lg:grid-cols-2'}>
                 <button
-                    className={'bg-black font-medium text-white uppercase py-4 px-6 tracking-wide text-xl block lg:hidden mb-6'}>Add
-                    to
-                    cart
+                    onClick={() => saveToCart(artwork)}
+                    className={'bg-black font-medium text-white uppercase py-4 px-6 tracking-wide text-xl block lg:hidden mb-6'}>
+                    Add to cart
                 </button>
                 <div className={''}>
-                    <p className={'font-bold text-md lg:text-lg mb-3'}><span className={'mr-1'}>About the</span><span>Samurai King Resting</span>
+                    <p className={'font-bold text-md lg:text-lg mb-3'}><span
+                        className={'mr-1'}>About the</span><span>{artwork.name}</span>
                     </p>
-                    <p className={'text-gray font-bold text-md lg:text-lg'}>Pets</p>
+                    <p className={'text-gray font-bold text-md lg:text-lg capitalize'}>{artwork.category}</p>
                     <p className={'text-gray leading-7 text-md lg:text-lg font-normal'}>
-                        So how did the classical Latin become so incoherent? According to McClintock, a 15th century
-                        typesetter likely scrambled part of Cicero's De Finibus in order to provide placeholder text to
-                        mockup various fonts for a type specimen book.So how did the classical Latin become so
-                        incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Cicero's
-                        De Finibus in order to provide placeholder
-
-                        text to mockup various fonts for a type specimen book.So how did the classical Latin become so
-                        incoherent? According to McClintock.
+                        {artwork.description}
                     </p>
                 </div>
                 <div className={'flex flex-col lg:items-end'}>
                     <p className={'font-bold text-md lg:text-lg mb-5'}>People also buy</p>
                     <div className={'grid grid-cols-3 gap-5 lg:gap-6 mb-xl'}>
-                        <div>
-                            <Image src={details} alt={'others'} />
-                        </div>
-                        <div>
-                            <Image src={details} alt={'others'} />
-                        </div>
-                        <div>
-                            <Image src={details} alt={'others'} />
-                        </div>
+                        {
+                            artwork.details && artwork.details.recommendations.map(recd => (
+                                <div key={`img-${recd.alt}`}
+                                     className={`${styles['recommended-image']} relative w-full`}>
+                                    <Image src={recd.src} alt={recd.alt} layout={'fill'} objectFit={'cover'}
+                                           objectPosition={'center'}/>
+                                </div>))
+                        }
                     </div>
                     <p className={'font-bold text-md lg:text-lg mb-3'}>Details</p>
                     <div>
