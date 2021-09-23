39375bdb4695a8a200d12e541bdfec4e83264036
Victory Garuba <garubav@gmail.com>
2021-09-23 23:50:54 +0100

    feat: import Sorting component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,36 +1,51 @@
-import Image from 'next/image'
 import Filters from '@/components/Filters'
 import PhotosGrid from '@/components/PhotosGrid'
+import Sorting from '@/components/Sorting'
 
-import sortIcon from '@images/sort.svg'
-import downArrow from '@images/down-arrow.svg'
-import toolbar from '@images/toolbar.svg'
+import {useEffect, useState, useCallback} from "react";
+import {useSelector} from "react-redux";
 
 const Gallery = () => {
+    const [artworks, setArtworks] = useState([])
+    const [sortBy, setSortBy] = useState({type: 'Name', asc: true})
+
+    const {products} = useSelector(state => state.products)
+
+
+    const sortProducts = useCallback((payload) => {
+        const items = [...payload]
+        if(!items.length) return []
+        if (sortBy.type === 'Name') {
+            return items.sort((a, b) => {
+                return sortBy.asc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+            })
+        } else {
+            return items.sort((a, b) => {
+                return sortBy.asc ? Number(a.price) < Number(b.price) : Number(a.price) > Number(b.price)
+            })
+        }
+    }, [sortBy])
+
+    useEffect(() => {
+        const sorted = sortProducts(products)
+        setArtworks([...sorted])
+    }, [products, sortProducts])
+
+    useEffect(() => {
+        const sorted = sortProducts(artworks)
+        setArtworks([...sorted])
+    }, [sortBy, sortProducts])
+
+    const handleSort = (key, value) => {
+        setSortBy({...sortBy, [key]: value})
+    }
+
     return (
         <section className={'pt-6 lg:pt-xl pb-4 lg:pb-4'}>
-            <section className={'flex justify-between'}>
-                <div>
-                    <p className={'text-md lg:text-1xl space-x-1'}>
-                        <span className={'font-bold'}>Photography</span><span className={'font-bold'}>/</span><span
-                        className={'text-grey font-normal'}>Premium Photos</span>
-                    </p>
-                </div>
-                <div>
-                    <div className={'hidden lg:flex'}>
-                        <p className={'flex items-center space-x-3 text-md lg:text-lg'}>
-                            <span className={'text-grey'}><Image src={sortIcon} alt={'sort icon'}/> Sort By</span>
-                            <span className={''}>Price <Image src={downArrow} alt={'down arrow'}/></span>
-                        </p>
-                    </div>
-                    <div className={'block lg:hidden'}>
-                        <button><Image src={toolbar} alt={'filter icon'}/></button>
-                    </div>
-                </div>
-            </section>
+            <Sorting sortBy={sortBy} callback={handleSort}/>
             <section className={'flex py-xl'}>
-                <Filters />
-                <PhotosGrid />
+                <Filters products={products}/>
+                <PhotosGrid artworks={artworks}/>
             </section>
         </section>
     )
