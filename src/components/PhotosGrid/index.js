11e64f63b01012669e0d9f961199864d23bddf14
Victory Garuba <garubav@gmail.com>
2021-09-23 23:50:13 +0100

    feat: plugin props data

--- src/components/PhotosGrid/index.js
+++ src/components/PhotosGrid/index.js
@@ -1,14 +1,13 @@
 import Artwork from "@/components/Artwork";
 import Pagination from "@/components/Pagination";
 
-const PhotosGrid = () => {
+const PhotosGrid = ({artworks}) => {
     return (
         <section className={'w-full lg:w-3/4'}>
             <div className={'grid grid-cols-1 lg:grid-cols-3 lg:px-3 gap-xl mb-2xl'}>
-                <Artwork/>
-                <Artwork/>
-                <Artwork/>
-                <Artwork/>
+                {
+                    artworks.map(art=><Artwork art={art} key={art.name} />)
+                }
             </div>
             <Pagination />
         </section>
