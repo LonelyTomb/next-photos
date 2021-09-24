be41834d51cd71b6473148f44a54568794926267
Victory Garuba <garubav@gmail.com>
2021-09-24 03:19:34 +0100

    feat: plugin pagination

--- src/components/PhotosGrid/index.js
+++ src/components/PhotosGrid/index.js
@@ -1,15 +1,22 @@
 import Artwork from "@/components/Artwork";
 import Pagination from "@/components/Pagination";
+import {useState, useEffect} from "react";
 
-const PhotosGrid = ({artworks}) => {
+const PhotosGrid = ({artworks, paging, handlePaging}) => {
+    const [paged, setPaged] = useState([])
+    useEffect(() => {
+        let sliced = artworks.slice((paging.currentPage - 1) * paging.perPage, (paging.currentPage) * paging.perPage)
+
+        setPaged([...sliced])
+    }, [paging, artworks])
     return (
         <section className={'w-full lg:w-3/4'}>
             <div className={'grid grid-cols-1 lg:grid-cols-3 lg:px-3 gap-xl mb-2xl'}>
                 {
-                    artworks.map(art=><Artwork art={art} key={art.name} />)
+                    paged.map(art => <Artwork art={art} key={art.name}/>)
                 }
             </div>
-            <Pagination />
+            <Pagination paging={paging} callback={handlePaging}/>
         </section>
     )
 }
