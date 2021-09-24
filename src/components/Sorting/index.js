295d4b91dac67df86630b0d8420dad3ebdc75907
Victory Garuba <garubav@gmail.com>
2021-09-24 09:32:53 +0100

    feat: add mobile filtering toggle

--- src/components/Sorting/index.js
+++ src/components/Sorting/index.js
@@ -1,10 +1,10 @@
-import {useState} from "react";
 import Image from "next/image";
+
 import sortIcon from "@images/sort.svg";
 import downArrow from "@images/down-arrow.svg";
 import toolbar from "@images/toolbar.svg";
 
-const Sorting = ({callback, sortBy}) => {
+const Sorting = ({callback, sortBy, setShowFilter}) => {
     const sortTypes = ['Price', 'Name']
 
     const handleChange = (key, value) => {
@@ -22,21 +22,27 @@ const Sorting = ({callback, sortBy}) => {
             <div>
                 <div className={'hidden lg:flex'}>
                     <div className={'flex items-center space-x-1 text-md lg:text-lg'}>
-                        <div className={'text-grey space-x-1'}>
-                            <button><Image src={sortIcon} alt={'sort icon'}/></button>
+                        <div className={'text-grey space-x-1 flex'}>
+                            <div>
+                                <Image src={sortIcon} alt={'sort icon'}/>
+                            </div>
                             <span>Sort By</span></div>
                         <select name="" id={'sort-by'} className={'appearance-none cursor-pointer'} value={sortBy.type}
                                 onChange={(e) => handleChange('type', e.target.value)}>
                             {
-                                sortTypes.map(sortType => (<option value={sortType} key={sortType} className={'capitalize'}>{sortType}</option>))
+                                sortTypes.map(sortType => (<option value={sortType} key={sortType}
+                                                                   className={'capitalize'}>{sortType}</option>))
                             }
                         </select>
-                        <button className={`${sortBy.asc ? 'rotate-180':''}`} onClick={()=> handleChange('asc', !sortBy.asc)}>
+                        <button className={`${sortBy.asc ? 'rotate-180' : ''}`}
+                                onClick={() => handleChange('asc', !sortBy.asc)}>
                             <Image src={downArrow} alt={'toggle arrow'}/></button>
                     </div>
                 </div>
                 <div className={'block lg:hidden'}>
-                    <button><Image src={toolbar} alt={'filter icon'}/></button>
+                    <button onClick={() => setShowFilter(true)}>
+                        <Image src={toolbar} alt={'filter icon'}/>
+                    </button>
                 </div>
             </div>
         </section>
