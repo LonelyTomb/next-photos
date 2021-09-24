150c8ddbc59c33bd9c4e9371a7b62833f3cfd0eb
Victory Garuba <garubav@gmail.com>
2021-09-24 09:32:45 +0100

    feat: handle price filtering

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,15 +1,27 @@
 import React, {useState, useEffect} from 'react'
+import PriceRange from "@/components/PriceRange";
 
-const Filters = ({products, callback, picked}) => {
+const Filters = ({products, callback, picked, priceLimit, priceCallback}) => {
     const [categories, setCategories] = useState([])
+    const [priceRange, setPriceRange] = useState({min: 0, max: 0})
+    const [price, setPrice] = useState(priceLimit)
 
     useEffect(() => {
         const filtered = new Set([...products.map(product => product.category)])
         setCategories([...filtered])
     }, [products])
 
+    useEffect(() => {
+        const prices = products.map(product => product.price).sort((a, b) => a - b)
+        setPriceRange({min: Math.floor(prices[0] || 0), max: Math.ceil(prices.pop() || 0)})
+    }, [products])
+
     const handleChange = (e) => {
         callback(e.target.value)
+    }
+    const handlePriceChange = (e) => {
+        setPrice(e.target.value)
+        priceCallback(e.target.value)
     }
     return (
         <section className={'w-1/4 pr-6 hidden lg:block'}>
@@ -19,7 +31,7 @@ const Filters = ({products, callback, picked}) => {
                     {
                         categories.map(category => (
                                 <div className={'form-group mb-6'} key={category}>
-                                    <input type="checkbox" name={category} value={category} id={`${category}-box`}
+                                    <input type="checkbox" name={'category'} value={category} id={`${category}-box`}
                                            checked={picked[category]}
                                            className={'border-3 border-black'} onChange={(e) => handleChange(e)}/>
                                     <label htmlFor={`${category}-box`} className={'ml-3 text-lg capitalize'}
@@ -31,16 +43,7 @@ const Filters = ({products, callback, picked}) => {
                 </form>
             </div>
             <hr style={{borderColor: '#C2C2C2'}} className={'border-t w-100 mb-6'}/>
-            <div className={'category'}>
-                <p className={'mb-6 text-lg font-bold'}>Price range</p>
-                <form>
-                    <div className={'form-group mb-xl'}>
-                        <input type="checkbox" name="category" value={'People'} id={'price-box'}
-                               className={'border-3 border-black'}/>
-                        <label htmlFor={'price-box'} className={'ml-3 text-lg'}>Lower than $20</label>
-                    </div>
-                </form>
-            </div>
+            <PriceRange price={price} priceRange={priceRange} handlePriceChange={handlePriceChange}/>
         </section>
     )
 }
