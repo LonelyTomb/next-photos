ac5bb0d42edb6299bf3b97d66bfba433ecc67c24
Victory Garuba <garubav@gmail.com>
2021-09-24 09:39:24 +0100

    feat: add clear button

--- src/components/PriceRange/index.js
+++ src/components/PriceRange/index.js
@@ -7,11 +7,21 @@ const PriceRange = ({price, priceRange, handlePriceChange}) => {
             <div className={'form-group mb-3 flex space-x-3 items-center'}>
                 <span>${priceRange.min}</span>
                 <input type="range" id={'pricing'} name={'pricing'}
-                       onChange={(e) => handlePriceChange(e)}
+                       onChange={(e) => handlePriceChange(e.target.value)}
                        min={priceRange.min} max={priceRange.max} value={price}/>
                 <span>${priceRange.max}</span>
             </div>
-            <div className={'text-lg'}>Lower than ${price}</div>
+            {
+                price > 0 && (
+                    <div className={'text-lg flex justify-between'}>
+                        <p>
+                            Lower than ${price}
+                        </p>
+                        <button type={'button'} onClick={() => handlePriceChange(0)} className={'hidden lg:block'}>Clear
+                        </button>
+                    </div>
+                )
+            }
         </form>
     </div>)
 }
